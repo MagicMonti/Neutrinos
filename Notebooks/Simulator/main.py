a689db474ce820faeb252427fb69e2de47729baf
Julian <julian@pop-os.localdomain>
2023-11-27 17:28:16 +0100

    added fitt

--- Notebooks/Simulator/main.py
+++ Notebooks/Simulator/main.py
@@ -1,49 +1,157 @@
 import numpy as np
-from trajectory import Geodesic
+from trajectory import Geodesic,Correction
 import matplotlib.pyplot as plt
 from scipy import constants as const
+from scipy.optimize import curve_fit
 
-#c = const.c
+c = const.c
 
-l = 1.9732705E-7
+M = 5.972E24 
+r_s = 2* const.G * M/c**2
+hbar = 1
+l_p = 1.9732705E-7
+m_p = 1.7826627E-36 
 
-r_start = 6371000 /l #earth radius in natural units
 
+#r_s in natrual units
+r_s = r_s/l_p
+
+r_start = 6371000 /l_p #earth radius in natural units
+
+m = 2.14E-37 
+
+beta = 0.98
+m = m / m_p
 
 x_0 = np.array([0, r_start, np.pi/2, 0])
 
 
-beta = 0.98
+simulation_length = 50000
+d_tau = 10131712
 
-u_0 = np.array([0, 0,0,beta/r_start]) 
+geodesic = Geodesic(beta, x_0, r_s, r_start)
+x_geo,u_geo = geodesic.get_path(simulation_length=simulation_length, dtau = d_tau)
+
+m_list = np.array([m*1E-5 *4, m*1E-5 *3.5 ,m*1E-5 *3, m*1E-5 *2.5, m*1E-5 *2, m*1E-5 *1])
+
+coef_m = []
+
+x_geo = x_geo.T
+y_geo=x_geo[1]*np.sin(x_geo[2])*np.sin(x_geo[3])
+x_geo=x_geo[1]*np.sin(x_geo[2])*np.cos(x_geo[3])
+
+print("mass of neutrino ", m)
+
+def deflection(y, C_1, C_2):
+    return C_1*y**2 + C_2*y
+
+def C_of_m(m,A,B):
+    return A*np.exp(-B*m)
+
+for m in m_list:
+    corr = Correction(x_0,m,r_s, x_geo, u_geo)
+    x,_ = corr.get_path(simulation_length=simulation_length, dtau = d_tau)
+    x = x.T
+
+    y=x[1]*np.sin(x[2])*np.sin(x[3])
+    x=x[1]*np.sin(x[2])*np.cos(x[3])
+
+    plt.plot(y*l_p/1000,(x-x_geo)*l_p/1000, label="m="+str(m))
+    coeffs, _ = curve_fit(deflection,y, (x-x_geo))
+    coef_m.append(coeffs)
 
 
-
-geo = Geodesic(u_0, x_0)
-
-(x_up,u,correction) = geo.calc_path(spin=1/2, simulation_length=500000,dtau = 10131712) 
-#x_down = geo.calc_path(spin=-1/2, simulation_length=500,dtau = 1E-8).T
-(x_geodesic,_,_) = geo.calc_path(spin=0, simulation_length=500000, dtau=10131712)
-
-np.savetxt("u.csv", u, delimiter=",")
-np.savetxt("correction.csv", correction, delimiter=",")
-np.savetxt("position.csv", x_up, delimiter=",")
-
-x_up = x_up.T
-x_geodesic = x_geodesic.T
-
-plt.scatter(x_geodesic[3][:-2]*r_start*l/1000, (np.pi/2-x_geodesic[2][:-2])*r_start*l, label="gedesic")
-plt.scatter(x_geodesic[3][:-2]*r_start*l/1000, (correction.T[2][0]-correction.T[2][:-2])*r_start*l * 1e12*2, label="correction")
-plt.scatter(x_up[3][:-2]*r_start*l/1000,(np.pi/2-x_up[2][:-2])*r_start*l, label="spin=1/2")
-plt.scatter(x_geodesic[3][:-2]*r_start*l/1000,(np.pi/2-x_up[2][:-2])*r_start*l, label="spin=1/2")
-#plt.scatter(x_up[3]*r_start*l/1000,(np.pi/2-x_up[2])*r_start*l, label="spin=1/2 up z-direction")
-#plt.plot(x_down[3]*r_start/1000,(np.pi/2-x_down[2])*r_start, label="spin=1/2 down -z-direction")
-
-#diff
-#plt.plot(x_geodesic[3]*r_start*l,(x_up[2]-x_geodesic[2]), label="difference")
-
-
-plt.xlabel("distance in km parallel to equator")
-plt.ylabel("distance in m normal to equator")
+#plt.plot(y_geo,x_geo)
 plt.legend()
-plt.savefig("trajectory_2.png")
+plt.xlabel("y in km")
+plt.ylabel("(x-x_geo) in km")
+plt.title("simulation at beta=0.98")
+plt.savefig("plot1.png")
+
+
+coef_m = np.array(coef_m)
+
+
+
+for i in range(2):
+    plt.clf()
+    plt.scatter(m_list,coef_m.T[i])
+    plt.title("C_"+str(i)+"(m)")
+    plt.xlabel("mass in natural units")
+    plt.ylabel("coefficent size")
+    plt.savefig("C_"+str(i)+"_of_m.png")
+
+plt.clf()
+for i,m in enumerate(m_list):
+    plt.plot(y*l_p/1000,deflection(y,coef_m[i][0], coef_m[i][1])*l_p/1000, label="m="+str(m))
+plt.legend()
+plt.title("fitt detla_x(y,m)")
+plt.xlabel("y in km")
+plt.ylabel("(x-x_geo) in km")
+plt.savefig("fitt_m.png")
+
+
+plt.clf()
+
+for i in range(2): #C_1 and C_2
+    coeffs_of_coeffs, _ = curve_fit(C_of_m,m_list,coef_m.T[i])
+    print("A: ", coeffs_of_coeffs[0])
+    print("B: ", coeffs_of_coeffs[1])
+    plt.plot(m_list, C_of_m(m_list, coeffs_of_coeffs[0], coeffs_of_coeffs[1]))
+    plt.title("fitt of C"+str(i))
+    plt.ylabel("coefficent size")
+    plt.xlabel("mass in natural units")
+    plt.savefig("coeff"+str(i)+".png")
+    plt.clf()
+
+y_helper = None
+
+
+beta_list = [0.5, 0.6, 0.7, 0.8,0.9]
+
+coef_beta = []
+for beta in beta_list:
+    geodesic = Geodesic(beta, x_0, r_s, r_start)
+    x_geo,u_geo = geodesic.get_path(simulation_length=simulation_length, dtau = d_tau)
+    corr = Correction(x_0,m,r_s, x_geo, u_geo)
+    x,_ = corr.get_path(simulation_length=simulation_length, dtau = d_tau)
+    x = x.T
+    x_geo = x_geo.T
+    y=x[1]*np.sin(x[2])*np.sin(x[3])
+    x=x[1]*np.sin(x[2])*np.cos(x[3])
+    y_helper = y
+    x_geo=x_geo[1]*np.sin(x_geo[2])*np.cos(x_geo[3])
+    plt.plot(y*l_p/1000,(x-x_geo)*l_p/1000, label="beta="+str(beta))
+    coeffs, _ = curve_fit(deflection,y, (x-x_geo))
+    coef_beta.append(coeffs)
+
+plt.legend()
+print(m)
+plt.xlabel("y in km")
+plt.ylabel("(x-x_geo) in km")
+plt.title("simulation at m="+str(m))
+plt.savefig("plot2.png")
+
+coef_beta = np.array(coef_beta)
+
+for i in range(2):
+    plt.clf()
+    plt.scatter(beta_list,coef_beta.T[i])
+    plt.title("C_"+str(i)+"(beta)")
+    plt.xlabel("beta")
+    plt.ylabel("coefficent size")
+    plt.savefig("C_"+str(i)+"_of_beta.png")
+
+plt.clf()
+for beta, coef in zip(beta_list, coef_beta):
+    geodesic = Geodesic(beta, x_0, r_s, r_start)
+    plt.plot(y_helper*l_p/1000,deflection(y_helper,coef[0], coef[1])*l_p/1000, label="beta="+str(beta))
+plt.legend()
+plt.title("fitt \delta y(x,\beta)")
+plt.xlabel("y in km")
+plt.ylabel("(x-x_geo) in km")
+plt.savefig("fitt2.png")
+
+
+
+
--- Notebooks/Simulator/trajectory.py
+++ Notebooks/Simulator/trajectory.py
@@ -1,40 +1,30 @@
 
-"""
-    ss-metric = mostly plus
-    the geodesic equation for 
-    dtheta/dtau = 0 and theta = pi/2 reads as follows 
-    a^\mu = [r_s* u_r* u_t/(r*(r - r_s)), (-2*r**3*u_\phi**2*(-r + r_s)**2 - r**2*r_s*u_r**2 + r_s*u_t**2*c**2*(r - r_s)**2)/(2*r**3*(r - r_s)), 0, 2*u_\phi*u_r/r]
-"""
-
 import numpy as np
 from scipy import constants as const
 
-c = const.c
-m = 2.14E-37 
-M = 5.972E24 
-r_s = 2* const.G * M/c**2
-hbar = 1
-l_p = 1.9732705E-7
-m_p = 1.7826627E-36 
 
-
-#r_s in natrual units
-r_s = r_s/l_p
-m = m / m_p
-
-m = m * 1E-8
-
-
-print("r_s", r_s)
-print("inverse mass", 1/m)
-
-
-class Geodesic():
-    def __init__(self, u_0, x_0):
-        self.u_0 = u_0
+class Trajctory():
+    def __init__(self, x_0, r_s):
         self.x_0 = x_0
+        self.r_s = r_s
 
-        self.corr = Correction()
+    def norm_of_vector(self, vec, x):
+        u_phi = vec[3]
+        u_theta = vec[2]
+        u_t = vec[0]
+        u_r = vec[1]
+        r = x[1]
+        theta = x[2]
+        return (r**3*(r - r_s)*(u_phi**2*np.sin(theta)**2 + u_theta**2) + r**2*u_r**2 - u_t**2*(r - r_s)**2)/(r*(r - r_s))
+
+    
+
+
+class Geodesic(Trajctory):
+    def __init__(self, beta, x_0,r_s, r_start):
+        super().__init__(x_0,r_s)
+        self.u_0 = u_0 = np.array([0, 0,0,beta/r_start]) 
+
 
     def normalize_u(self,u,x):
         """noramlizes u to u^2 = 1"""
@@ -44,16 +34,8 @@ class Geodesic():
         u_phi = u[3]
         u_r = u[1]
         #u_t = np.sqrt(r*(c**2*r - c**2*r_s + r**3*u_phi**2*np.sin(theta)**2 + r**3*u_theta**2 - r**2*r_s*u_phi**2*np.sin(theta)**2 - r**2*r_s*u_theta**2 + r*u_r**2))/(r - r_s)
-        u_t = np.sqrt(r*(r - r_s + r**3*u_phi**2*np.sin(theta)**2 + r**3*u_theta**2 - r**2*r_s*u_phi**2*np.sin(theta)**2 - r**2*r_s*u_theta**2 + r*u_r**2))/(r - r_s)
+        u_t = np.sqrt(r*(r - self.r_s + r**3*u_phi**2*np.sin(theta)**2 + r**3*u_theta**2 - r**2*self.r_s*u_phi**2*np.sin(theta)**2 - r**2*self.r_s*u_theta**2 + r*u_r**2))/(r - self.r_s)
         return np.array([u_t, u_r, u_theta, u_phi])
-
-    # def acc(self,u,x):
-    #     r = x[1]
-    #     theta = x[2]
-    #     u_theta = u[2]
-    #     u_phi = u[3]
-    #     u_r = u[1]
-    #     return hbar_m*np.array([0, -r_s*u_theta*np.sqrt(1 - r_s)/r**2, r_s*u_r*np.sqrt(1 - r_s)/(r**3*(r - r_s)), 0])
 
     def du_dtau(self,u,x):
         r = x[1]
@@ -67,123 +49,73 @@ class Geodesic():
 
         #print("x : ", x, "\t", "u :", u)
 
-        a_0 = r_s*u_r*u_t/(r*(r - r_s))
-        a_1 = (-2*r**3*(-r + r_s)**2*(u_phi**2*np.sin(theta)**2 + u_theta**2) - r**2*r_s*u_r**2 + r_s*u_t**2*(r - r_s)**2)/(2*r**3*(r - r_s))
+        a_0 = self.r_s*u_r*u_t/(r*(r - self.r_s))
+        a_1 = (-2*r**3*(-r + self.r_s)**2*(u_phi**2*np.sin(theta)**2 + u_theta**2) - r**2*self.r_s*u_r**2 + self.r_s*u_t**2*(r - self.r_s)**2)/(2*r**3*(r - self.r_s))
         a_2 = -u_phi**2*np.sin(2*theta)/2 + 2*u_theta*u_r/r
         a_3 = 2*u_phi*(r*u_theta / np.tan(theta) + u_r)/r
         return np.array([-a_0,-a_1,-a_2,-a_3])
 
-    def norm_of_vector(self, vec, x):
-
-        u_phi = vec[3]
-        u_theta = vec[2]
-        u_t = vec[0]
-        u_r = vec[1]
-        r = x[1]
-        theta = x[2]
-
-        return (r**3*(r - r_s)*(u_phi**2*np.sin(theta)**2 + u_theta**2) + r**2*u_r**2 - u_t**2*(r - r_s)**2)/(r*(r - r_s))
-
-    def calc_path(self, simulation_length=10000, dtau=0.0001, spin=0):
-
-        #t,r,theta, phi
+    def get_path(self, simulation_length=10000, dtau=1000):
         u_0 = self.normalize_u(self.u_0, self.x_0)
-
-        x = np.ones((simulation_length+1,4))
-        x_geo = np.ones((simulation_length+1,4))
         u = np.ones((simulation_length+1,4))
+        x = np.ones((simulation_length+1,4))
 
         x[0] = self.x_0
         u[0] = self.u_0
 
-        x_geo[0] = self.x_0
+        for i in range(simulation_length):
+            u[i] = self.normalize_u(u[i], x[i])
+            u[i+1] =  self.du_dtau(u[i], x[i])*dtau + u[i]
+            x[i+1] =  u[i]*dtau + x[i]
 
-        correction = np.ones((simulation_length+1,4))
+        return x,u
+   
+class Correction(Trajctory):
+    def __init__(self, x_0, m,r_s,x_geo,u_geo):
+        super().__init__(x_0,r_s)
+        #self.beta = beta
+        self.m = m
+        self.x_geo = x_geo
+        self.u_geo = u_geo
+    
+    def get_path(self, simulation_length=10000, dtau=1000):
+        x = np.ones((simulation_length+1,4))
+        v = np.ones((simulation_length,4))
+        x[0] = self.x_0
 
-        if spin==1/2:
-            v = np.ones((simulation_length,4))
-            for i in range(simulation_length):
-                #constrain u^mu u_mu = -1 for all tau
-                # => overwrite u_t component
+        for i in range(simulation_length):
+            v[i] = self.u_geo[i] + self._up_correction(x[i]) 
+            x[i+1] =  v[i]*dtau + x[i]
+      
+        return x,v
 
-                u[i] = self.normalize_u(u[i], x_geo[i])
-                #print(u[i])
-
-                #print(self.normalize_u(u[i], x[i]))
-                
-                #print(self.norm_of_vector(u[i], x[i])/c**2,self.norm_of_vector(self.corr.up_correction(x[i]),x[i])/c**2)
-                v[i] = u[i] + self.corr.up_correction(x[i]) 
-
-                correction[i] = self.corr.up_correction(x[i]) 
-
-                
-                u[i+1] =  self.du_dtau(u[i], x_geo[i])*dtau + u[i]
-                #print(u[i])
-                x[i+1] =  v[i]*dtau + x[i]
-                x_geo[i+1] = u[i]*dtau + x_geo[i]
-
-                #print(self.acc(u[i], x[i]))
-
-                #print(self.norm_of_vector(u[i], x[i]), self.norm_of_vector(v[i], x[i]))
-        if spin==-1/2:
-            v = np.ones((simulation_length+1,4))
-            v[0] = self.normalize_u(u[0], x[0]) #+ self.corr.down_correction(x[0])
-            #print(self.normalize_u(u[0], x[0]), self.corr.down_correction(x[0]))
-            for i in range(simulation_length):
-
-               pass
-
-        elif spin==0:
-           
-            for i in range(simulation_length):
-
-                u[i] = self.normalize_u(u[i], x[i])
-
-                
-                u[i+1] =  self.du_dtau(u[i], x[i])*dtau + u[i]
-
-           
-                x[i+1] =  u[i]*dtau + x[i]
-                
-
-        return (x,u,correction)
-
-class Correction():
-    def __init__(self):
-        pass
-
-    # def __sigma(self,x):
-    #     r = x[1]
-    #     f = 1 - r_s/r
-    #     return np.array([[0, 0, 0, 0], [0, 0, sqrt(f)/r, 0], [0, -sqrt(f)/r, 0, 0], [0, 0, 0, 0]])
-
-    def up_correction(self,x):
+    def _up_correction(self,x):
         r = x[1]
         theta = x[2]
 
         #print((2*r_s*(-r + r_s)**4*np.sin(theta) + r_s*(r - r_s)**2*(r*(r - r_s) + (-r + r_s)**2)*np.sin(theta) - 2*np.sqrt((r - r_s)/r)*(-r + r_s)**4*(2*r*np.sqrt(1 - r_s/r)*np.sin(theta) + np.sin(theta) + 1))/(8*r**3*(-r + r_s)**5*np.sin(theta)) * hbar/m)
-        return np.array([0, hbar*(r - r_s)*np.cos(theta)/(4*m*r), hbar*(-2*r**2*np.sqrt(1 - r_s/r)*np.sin(theta) - 2*r**2*np.sqrt(1 - r_s/r) + r_s)/(8*m*r**4), 0])
+        return np.array([0, (r - self.r_s)*np.cos(theta)/(4*self.m*r), (-2*r**2*np.sqrt(1 - self.r_s/r)*np.sin(theta) - 2*r**2*np.sqrt(1 - self.r_s/r) + self.r_s)/(8*self.m*r**4), 0])
         #return hbar/m * np.array([0, -(r - r_s)*(np.sin(theta) + 1)*np.cos(theta)/(4*r*np.sin(theta)**2), (2*r_s*(-r + r_s)**4*np.sin(theta) + r_s*(r - r_s)**2*(r*(r - r_s) + (-r + r_s)**2)*np.sin(theta) - 2*np.sqrt((r - r_s)/r)*(-r + r_s)**4*(2*r*np.sqrt(1 - r_s/r)*np.sin(theta) + np.sin(theta) + 1))/(8*r**3*(-r + r_s)**5*np.sin(theta)), 0])
 
     
-    def up_x_correction(self,x):
+    # def _up_x_correction(self,x):
 
-        r = x[1]
-        theta = x[2]
+    #     r = x[1]
+    #     theta = x[2]
 
-        return np.array([0, 0, 0, hbar*(np.sin(theta) + 1)*np.cos(theta)/(4*m*r**2*np.sin(theta)**4)])
+    #     return np.array([0, 0, 0, hbar*(np.sin(theta) + 1)*np.cos(theta)/(4*m*r**2*np.sin(theta)**4)])
 
-    def up_y_correction(self,x):
+    # def _up_y_correction(self,x):
 
-        r = x[1]
-        theta = x[2]
+    #     r = x[1]
+    #     theta = x[2]
         
-        return np.array([0, 0, 0, hbar*(-4*r**2 + 12*r*r_s - 2*r*np.sqrt(1 - r_s/r) - 2*r*np.sqrt(1 - r_s/r)/np.sin(theta) - 7*r_s**2 + 2*r_s*np.sqrt(1 - r_s/r) + 2*r_s*np.sqrt(1 - r_s/r)/np.sin(theta))/(8*m*r**3*(r**2 - 2*r*r_s + r_s**2)*np.sin(theta)**2)])
+    #     return np.array([0, 0, 0, hbar*(-4*r**2 + 12*r*r_s - 2*r*np.sqrt(1 - r_s/r) - 2*r*np.sqrt(1 - r_s/r)/np.sin(theta) - 7*r_s**2 + 2*r_s*np.sqrt(1 - r_s/r) + 2*r_s*np.sqrt(1 - r_s/r)/np.sin(theta))/(8*m*r**3*(r**2 - 2*r*r_s + r_s**2)*np.sin(theta)**2)])
     
-    def down_correction(self,x):
-        r = x[1]
-        theta = x[2]
+    # def _down_correction(self,x):
+    #     r = x[1]
+    #     theta = x[2]
 
-        pass
-        #return hbar_m*np.array([0, 0.25*c**2*(-r + r_s)*np.sin(theta)**2*np.cos(theta)/r, - c**2*(r_s*((r - r_s)/r)**(9/2)*(0.25*r**3 - 0.125*c**2*(r - r_s)**3) + 0.25*(r - r_s)**7*(np.sin(theta)**3 + 1))/(r**6*((r - r_s)/r)**(9/2)*(r - r_s)), 0])
+    #     pass
+    #     #return hbar_m*np.array([0, 0.25*c**2*(-r + r_s)*np.sin(theta)**2*np.cos(theta)/r, - c**2*(r_s*((r - r_s)/r)**(9/2)*(0.25*r**3 - 0.125*c**2*(r - r_s)**3) + 0.25*(r - r_s)**7*(np.sin(theta)**3 + 1))/(r**6*((r - r_s)/r)**(9/2)*(r - r_s)), 0])
 
